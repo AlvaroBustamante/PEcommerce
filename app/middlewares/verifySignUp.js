97f0aaab6d8e775aabf7c674d1a65590d142bbc3
zAlvaroD <a20101263@idat.edu.pe>
2024-08-03 02:26:02 -0500

    Avance 2.2

--- app/middlewares/verifySignUp.js
+++ app/middlewares/verifySignUp.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 const ROLES = db.ROLES;
-const User = db.user;
+const {User} = require('../models/user.model');
 
 checkDuplicateUsernameOrEmail = (req, res, next) => {
     // Username
